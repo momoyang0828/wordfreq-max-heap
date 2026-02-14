f6879f9bc546592d88a68b186188e19fdcc86ba1
momoyang0828 <momoyang0828@gmail.com>
2026-02-14 01:22:22 -0800

    Initialize WordFreq array with survey phrase frequency data

--- MaxHeapWordFreq.java
+++ MaxHeapWordFreq.java
@@ -13,3 +13,16 @@ public class MaxHeapWordFreq {
 
     }
 }
+
+WordFreq[] arr = {
+    new WordFreq("happy", 400),
+    new WordFreq("satisfied", 100),
+    new WordFreq("neutral", 300),
+    new WordFreq("would buy again", 200),
+    new WordFreq("terrible", 160),
+    new WordFreq("inconvenient", 900),
+    new WordFreq("difficult to use", 100),
+    new WordFreq("easy to use", 140),
+    new WordFreq("would recommend to friends", 800),
+    new WordFreq("visit the store", 700)
+};
