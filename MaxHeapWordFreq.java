fc9341a2394b70e0f8abdeb7ffd31d94ba5c9fc5
momoyang0828 <momoyang0828@gmail.com>
2026-02-14 01:21:56 -0800

    Create WordFreq class and basic program structure

---
+++ MaxHeapWordFreq.java
@@ -0,0 +1,15 @@
+class WordFreq {
+    String word;
+    int frequency;
+
+    public WordFreq(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+}
+
+public class MaxHeapWordFreq {
+    public static void main(String[] args) {
+
+    }
+}
