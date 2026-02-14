7415e44e678bb56b92f9f7024035454a49ecdd54
momoyang0828 <momoyang0828@gmail.com>
2026-02-14 01:24:18 -0800

    Add helper methods for heap index calculation and swap

--- MaxHeapWordFreq.java
+++ MaxHeapWordFreq.java
@@ -9,20 +9,37 @@ class WordFreq {
 }
 
 public class MaxHeapWordFreq {
+
+    static int heapSize;
+
     public static void main(String[] args) {
 
+        WordFreq[] arr = {
+            new WordFreq("happy", 400),
+            new WordFreq("satisfied", 100),
+            new WordFreq("neutral", 300),
+            new WordFreq("would buy again", 200),
+            new WordFreq("terrible", 160),
+            new WordFreq("inconvenient", 900),
+            new WordFreq("difficult to use", 100),
+            new WordFreq("easy to use", 140),
+            new WordFreq("would recommend to friends", 800),
+            new WordFreq("visit the store", 700)
+        };
+
+    }
+
+    static int left(int i) {
+        return 2 * i + 1;
+    }
+
+    static int right(int i) {
+        return 2 * i + 2;
+    }
+
+    static void swap(WordFreq[] arr, int i, int j) {
+        WordFreq temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
-
-WordFreq[] arr = {
-    new WordFreq("happy", 400),
-    new WordFreq("satisfied", 100),
-    new WordFreq("neutral", 300),
-    new WordFreq("would buy again", 200),
-    new WordFreq("terrible", 160),
-    new WordFreq("inconvenient", 900),
-    new WordFreq("difficult to use", 100),
-    new WordFreq("easy to use", 140),
-    new WordFreq("would recommend to friends", 800),
-    new WordFreq("visit the store", 700)
-};
